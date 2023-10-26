edc2c86cdef32408d2151865693e92ace7f76311
JosMiguelMM2 <josemmendez@ucundinamarca.edu.co>
2023-10-25 21:33:43 -0500

    S

--- src/routers/index.routes.ts
+++ src/routers/index.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { conection } from '../config/db';
 
 const router = Router();
-
+//ESTO ES UNA PRUEBA, NO ES NECESARIO
 router.get('/ping', (req, res) => {
   req.body;
   console.log('someone pinged her');
