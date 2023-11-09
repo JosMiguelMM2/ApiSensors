1c179f34574c6ba6f93a1d4d49d3ddfe97ec1ed8
JosMiguelMM2 <josemmendez@ucundinamarca.edu.co>
2023-11-08 20:20:46 -0500

    partially fixed bugs

--- src/controllers/PatchOrders/patchOrders.controller.ts
+++ src/controllers/PatchOrders/patchOrders.controller.ts
@@ -1,15 +1,21 @@
 import { Request, Response } from 'express';
-import { patchOrdersService}  from '../../services/patchOrders/patchOrdersService';
+import { patchOrdersService } from '../../services/patchOrders/patchOrdersService';
 
 export default async (req: Request, res: Response) => {
   const OrderID = req.params.id;
-  const OrderIDInt: Number = parseInt(OrderID);
-  const statusLed = req.body.statusLed;
-  if (!statusLed) {
-    return res.status(400).json({ error: 'Faltan datos requeridos' });
+  const OrderIDInt: number = parseInt(OrderID);
+  if (OrderIDInt > 5 && OrderIDInt <= 7) {
+  } else if (OrderIDInt <= 5 && OrderIDInt > 0) {
+    const statusLed = req.body.statusLed;
+    if (!statusLed) {
+      return res.status(400).json({ error: 'Faltan datos requeridos' });
+    }
+    console.log(OrderIDInt);
+    console.log(statusLed);
+    const ejecution = await patchOrdersService(OrderIDInt, statusLed);
+    res.send('Orden actualizada ' + ejecution);
+  }else{
+    res.status(404).json({ error: 'Orden no encontrada' });
   }
-  console.log(OrderIDInt);
-  console.log(statusLed);
-  const ejecution=await patchOrdersService(OrderIDInt, statusLed);
-  res.send('Orden actualizada' + ejecution);
+
 };
---
+++ src/services/patchServoOrders/patchServoOrders.server.ts
@@ -0,0 +1,18 @@
+import { conection } from '../../config/db';
+
+async function patchOrdersDServosService(idNew: Number, degreesNew: Number) {
+  try {
+    const client = await conection();
+    const result = await client
+      .db('Home')
+      .collection('Status_orders')
+      .updateOne({ _id: idNew }, { $set: { degrees: degreesNew } });
+    client.close();
+    const resulT= result.acknowledged;
+    return resulT;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+}
+export { patchOrdersDServosService };
