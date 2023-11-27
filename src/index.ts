75bf0e3effc8a0e5557bea4bf5ddc2f157d941e4
JosMiguelMM2 <josemmendez@ucundinamarca.edu.co>
2023-11-27 10:29:24 -0500

    url cors

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,16 @@ import config  from './dotenv/config';
 const app = express();
 const PORT = config().databaseMongoDB.port || 3000;
 
-//const url = config().fontend.url;
-/*.use(
+const url = config().fontend.url;
+app.use(
   cors({
     origin: `${url}`,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
   })
-);*/
+);
 
-//ggt
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 app.use(indexroutes);
 
